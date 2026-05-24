5dcaa9b9b9aa8ecd6d6902d122aa178303b2e028
Benxiang Ge <gebenxiang@gmail.com>
2026-05-24 23:11:19 +0800

    convert mlirgen to semantic types
    Change-Id: Ife13ffff03b1a4b11003999966573ddd1f07ce4c

---
+++ include/cherry/MLIRGen/TypeConverter.h
@@ -0,0 +1,35 @@
+#ifndef CHERRY_MLIRGEN_TYPECONVERTER_H
+#define CHERRY_MLIRGEN_TYPECONVERTER_H
+
+#include "cherry/Basic/Types.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/Types.h"
+
+namespace mlir {
+class MemRefType;
+} // namespace mlir
+
+namespace cir {
+class RecordType;
+} // namespace cir
+
+namespace cherry {
+
+class MLIRTypeConverter {
+public:
+  explicit MLIRTypeConverter(mlir::OpBuilder& builder) : _builder(builder) {}
+
+  auto convert(const Type *type) const -> mlir::Type;
+
+private:
+  auto convert(const BuiltinType& type) const -> mlir::Type;
+  auto convert(const ListType& type) const -> mlir::MemRefType;
+  auto convertListElement(const BuiltinType& type) const -> mlir::Type;
+  auto convert(const StructType& type) const -> cir::RecordType;
+
+  mlir::OpBuilder& _builder;
+};
+
+} // namespace cherry
+
+#endif // CHERRY_MLIRGEN_TYPECONVERTER_H
